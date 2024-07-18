ef85870cb009fbf3dd470fd3656dec7775238360
Derek Gooding <djgooding@live.com>
2024-07-18 14:15:47 -0400

    More of the same

--- Restaurant-Menu-Systemv2/EditOrderItem.cs
+++ Restaurant-Menu-Systemv2/EditOrderItem.cs
@@ -1,14 +1,9 @@
 ﻿namespace Restaurant_Menu_System_V3;
 
-public class EditOrderItem
+public class EditOrderItem(OrderInputAndOptions orderChoice)
 {
     // private field to store the order choices, allowing for edits to the current order
-    private OrderInputAndOptions _orderChoice;
-
-    public EditOrderItem(OrderInputAndOptions orderchoice)
-    {
-        _orderChoice = orderchoice;
-    }
+    private readonly OrderInputAndOptions _orderChoice = orderChoice;
 
     // prompts user to ask if they would like to edit their order
     // displays options for user to edit and takes them back to their spot in the menu
@@ -23,28 +18,28 @@ public class EditOrderItem
         // need to resolve this issue at some point after building out rest of application.
         // need to add ability to completely remove entree and choices from list if user changes their mind.
         Action[] orderOptions =
-        {
+        [
            _orderChoice.ChooseEntree,
            _orderChoice.ChooseTortilla,
            _orderChoice.ChooseProtein,
            _orderChoice.ChooseRice,
            _orderChoice.ChooseBeans,
            _orderChoice.ChooseAddOns
-        };
+        ];
 
         MenuOption[] options =
-        {
+        [
             new MenuOption() { ItemName = "Edit Entrée Choice" },
             new MenuOption() { ItemName = "Edit Tortilla Choice" },
             new MenuOption() { ItemName = "Edit Protein Choice" },
             new MenuOption() { ItemName = "Edit Rice Choice" },
             new MenuOption() { ItemName = "Edit Bean Choice" },
             new MenuOption() { ItemName = "Edit Add-On Choices" },
-        };
+        ];
 
         OrderInputAndOptions.DisplayOrderOptions("CHOOSE WHICH MENU ITEM YOU WOULD LIKE TO EDIT:", options);
 
-        while (true)
+        do
         {
             int choice = OrderInputAndOptions.GetIntegerInput("Enter the number for what you would like to edit. ", 1, orderOptions.Length) - 1;
 
@@ -78,11 +73,7 @@ public class EditOrderItem
             }
 
             orderOptions[choice]();
-
-            if (!OrderInputAndOptions.GetYesNoResponse("Would you like to make another edit?"))
-            {
-                break;
-            }
         }
+        while (OrderInputAndOptions.GetYesNoResponse("Would you like to make another edit?"));
     }
 }
--- Restaurant-Menu-Systemv2/Program.cs
+++ Restaurant-Menu-Systemv2/Program.cs
@@ -1,4 +1,4 @@
-﻿using Restaurant_Menu_System_V3;
+﻿namespace Restaurant_Menu_System_V3;
 
 public static class Program
 {
