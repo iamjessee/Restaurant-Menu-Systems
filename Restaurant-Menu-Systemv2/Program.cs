c6f522979f0eb83b815c4c780ad82ace4f908442
Derek Gooding <djgooding@live.com>
2024-07-18 14:52:58 -0400

    missed a redundant param

--- Restaurant-Menu-Systemv2/Program.cs
+++ Restaurant-Menu-Systemv2/Program.cs
@@ -44,6 +44,6 @@ public static class Program
         menuOptionAdder.AddMoreEntrees();
 
         // displays user receipt
-        customerReceipt.DisplayReceipt(customerName);
+        customerReceipt.DisplayReceipt();
     }
 }
